0c05b9103c0b42b3a3176ad6e33d2b907f5d2ac5
Cian Butler <ciandevelops@gmail.com>
2022-06-07 18:15:02 -0400

    hard coded and basic styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+
   return (
-    <div className="App">
-    <h1>Hello World!</h1>
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Toronto</p>
+          </div>
+          <div className="temp">
+            <h1>22C</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+
+        <div className="bottom">
+          <div className="feels">
+            <p>25C</p>
+          </div>
+          <div className="humidity">
+            <p>80%</p>
+          </div>
+          <div className="wind">
+            <p>10 MPH</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
