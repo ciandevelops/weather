2b910bd616c7495c8e13a8c485cb29bba2a50fe3
Cian Butler <ciandevelops@gmail.com>
2022-06-07 20:27:06 -0400

    rounded values

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
             <p>{data.name}</p>
           </div>
           <div className="temp">
-            {data.main ? <h1>{data.main.temp}°C</h1> : null}
+            {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
           </div>
           <div className="description">
             {data.weather ? <p>{data.weather[0].main}</p> : null}
@@ -45,7 +45,7 @@ function App() {
           <div className="bottom">
             <div className="feels">
               {data.main ? (
-                <p className="bold">{data.main.feels_like}°C</p>
+                <p className="bold">{data.main.feels_like.toFixed()}°C</p>
               ) : null}
               <p>Feels Like</p>
             </div>
@@ -54,7 +54,9 @@ function App() {
               <p>Humidity</p>
             </div>
             <div className="wind">
-              {data.wind ? <p className="bold">{data.wind.speed} MPH</p> : null}
+              {data.wind ? (
+                <p className="bold">{data.wind.speed.toFixed()} KM/H</p>
+              ) : null}
               <p>Wind Speed</p>
             </div>
           </div>
